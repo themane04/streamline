7ef5995f945d52375e7088e653377dd5d23aae57
mane <tomevmarjan33@gmail.com>
2024-11-17 23:10:10 +0100

    initial commit

---
+++ Streamline/MainActivity.cs
@@ -0,0 +1,11 @@
+namespace Streamline;
+
+[Activity(Label = "@string/app_name", MainLauncher = true)]
+public class MainActivity : Activity
+{
+    protected override void OnCreate(Bundle? savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+        SetContentView(Resource.Layout.activity_main);
+    }
+}
