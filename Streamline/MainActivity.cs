11faef1d060f05aa5d7a0f69e09ae75852a095a0
mane <tomevmarjan33@gmail.com>
2024-11-18 13:42:46 +0100

    - redirect functional

--- Streamline/MainActivity.cs
+++ Streamline/MainActivity.cs
@@ -1,3 +1,5 @@
+using Android.Views;
+
 namespace Streamline;
 
 [Activity(Label = "@string/app_name", MainLauncher = true)]
@@ -7,6 +9,44 @@ public class MainActivity : Activity
     {
         base.OnCreate(savedInstanceState);
         ActionBar?.Hide();
+        GoToSignIn();
+    }
+
+    private void GoToSignIn()
+    {
         SetContentView(Resource.Layout.sign_in);
+        TextView signUpTextView = FindViewById<TextView>(Resource.Id.signup_text);
+        signUpTextView?.SetOnClickListener(new ClickListener(this, "signUp"));
+    }
+
+    private void GoToSignUp()
+    {
+        SetContentView(Resource.Layout.sign_up);
+        TextView signInTextView = FindViewById<TextView>(Resource.Id.signin_text);
+        signInTextView?.SetOnClickListener(new ClickListener(this, "signIn"));
+    }
+
+    private class ClickListener : Java.Lang.Object, View.IOnClickListener
+    {
+        private readonly MainActivity _activity;
+        private readonly string _action;
+
+        public ClickListener(MainActivity activity, string action)
+        {
+            _activity = activity;
+            _action = action;
+        }
+
+        public void OnClick(View? v)
+        {
+            if (_action == "signIn")
+            {
+                _activity.GoToSignIn();
+            }
+            else if (_action == "signUp")
+            {
+                _activity.GoToSignUp();
+            }
+        }
     }
 }
