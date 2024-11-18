2129be614ad8b455cc6a0e0348e748a74e82b9ab
mane <tomevmarjan33@gmail.com>
2024-11-18 02:40:51 +0100

    sign in page design done

--- Streamline/MainActivity.cs
+++ Streamline/MainActivity.cs
@@ -1,5 +1,3 @@
-using Android.Views;
-
 namespace Streamline;
 
 [Activity(Label = "@string/app_name", MainLauncher = true)]
@@ -10,7 +8,5 @@ public class MainActivity : Activity
         base.OnCreate(savedInstanceState);
         ActionBar?.Hide();
         SetContentView(Resource.Layout.sign_in);
-        View rootView = FindViewById(Android.Resource.Id.Content);
-        rootView.SetBackgroundColor(Android.Graphics.Color.ParseColor("#FEF3E2"));
     }
 }
