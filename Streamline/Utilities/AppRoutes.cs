cce5b4425054031486e251480def74cb7ed1f687
mane <tomevmarjan33@gmail.com>
2024-12-16 12:34:24 +0100

    - created AppRoutes to have easy access to the routes - transformed the <a> tags in SignIn and SignUp to buttons since MudButtons work now  - moved the CSS for the button in the mud.blazor.classes.css - added the icons for the navigation active and inactive - created all the pages just empty - created a MainLayout.razor for the navigation at the bottom - created the Navigation.razor -

---
+++ Streamline/Utilities/AppRoutes.cs
@@ -0,0 +1,17 @@
+﻿namespace Streamline.Utilities;
+
+public static class AppRoutes
+{
+    public const string LoginUrl = "/";
+    public const  string SignupUrl = "/signup";
+    public const  string HomeUrl = "/home";
+    public const  string BookmarkUrl = "/bookmark";
+    public const  string FavoritesUrl = "/favorites";
+    public const  string ProfileUrl = "/profile";
+
+    public static readonly List<string> ExcludedRoutes = new()
+    {
+        LoginUrl,
+        SignupUrl
+    };
+}
