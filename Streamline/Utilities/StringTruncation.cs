b4a068b5d80f290b9ef43b9b4a6e70b7fc45361e
mane <tomevmarjan33@gmail.com>
2024-12-18 17:24:47 +0100

    - movie item improvements - all around responsivness improvements - movie detail additions

---
+++ Streamline/Utilities/StringTruncation.cs
@@ -0,0 +1,10 @@
+﻿namespace Streamline.Utilities;
+
+public class StringTruncation
+{
+    public static string Truncate(string? input, int maxLength)
+    {
+        if (string.IsNullOrEmpty(input)) return string.Empty;
+        return input.Length > maxLength ? input.Substring(0, maxLength) + "..." : input;
+    }
+}
