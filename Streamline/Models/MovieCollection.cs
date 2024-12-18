dc5c8fd9afcff792044ad25751e286b3db43f134
mane <tomevmarjan33@gmail.com>
2024-12-18 20:59:40 +0100

    - added another information on the detail page for the movie - some code clearance

--- Streamline/Models/MovieCollection.cs
+++ Streamline/Models/MovieCollection.cs
@@ -4,11 +4,12 @@ namespace Streamline.Models;
 
 public class MovieCollection
 {
-    [JsonPropertyName("id")] public int Id { get; set; }
+    public string FullPosterPath => $"https://image.tmdb.org/t/p/w500{PosterPath}";
+    public string FullBackdropPath => $"https://image.tmdb.org/t/p/w500{BackdropPath}";
 
+    [JsonPropertyName("id")] public int Id { get; set; }
     [JsonPropertyName("name")] public string Name { get; set; } = string.Empty;
 
     [JsonPropertyName("poster_path")] public string? PosterPath { get; set; }
-
     [JsonPropertyName("backdrop_path")] public string? BackdropPath { get; set; }
 }
